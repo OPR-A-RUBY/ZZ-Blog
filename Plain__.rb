572a80b3bafb670a21661940b973b48d0c0a4d52
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-14 22:37:47 +0400

    View routes
    Просматриваем существующие маршруты.

--- Plain__.rb
+++ Plain__.rb
@@ -1,70 +1,101 @@
-1. Устанавливаем Rais
-# gem install rails
+____ 1. Устанавливаем Rais
 
-2. Переходим в папку, где будет создаваться папка с нашим приложением внутри. И вводим команду создания нового приложенеия на RAILS
-# rails new ZZ-Blog
-Будет создана папка ZZ_Blog, в ней ещё куча папок и файлов. Затем автоматически запуститься установка необходимых гемов: bundle install
-и потом ещё куча зависимостей. Всё автоматически.
-Процесс не быстрый придётся подождать ...
-13.05.2020 - у меня получилось 16.886 файлов и 2985 папок
+gem install rails
 
-3. Дальше смотрим, на запус RAILS т.е. вводим коменду:
-# rails server
-будет примерно такой вывод в терминале:
-=> Booting Puma
-=> Rails 6.0.3 application starting in development 
-=> Run `rails server --help` for more startup options
-Puma starting in single mode...
-* Version 4.3.3 (ruby 2.6.0-p0), codename: Mysterious Traveller
-* Min threads: 5, max threads: 5
-* Environment: development
-* Listening on tcp://127.0.0.1:3000
-* Listening on tcp://[::1]:3000
-Use Ctrl-C to stop
+____ 2. Переходим в папку, где будет создаваться папка с нашим приложением внутри. И вводим команду создания нового приложенеия на RAILS
 
-По адресу http://localhost:3000/
-смотрим стартовую картинку RUBY on RAILS
-здесь же видны версии Rails и Ruby
-у меня вот так:
-Rails version: 6.0.3
-Ruby version: ruby 2.6.0p0 (2018-12-25 revision 66547) [x86_64-linux]
+rails new ZZ-Blog
+
+# Будет создана папка ZZ_Blog, в ней ещё куча папок и файлов. Затем автоматически запуститься установка необходимых гемов: bundle install
+# и потом ещё куча зависимостей. Всё автоматически.
+# Процесс не быстрый придётся подождать ...
+# 13.05.2020 - у меня получилось 16.886 файлов и 2985 папок
+
+____ 3. Дальше смотрим, на запус RAILS т.е. вводим коменду:
+
+rails server
+
+# будет примерно такой вывод в терминале:
+# => Booting Puma
+# => Rails 6.0.3 application starting in development 
+# => Run `rails server --help` for more startup options
+# Puma starting in single mode...
+# * Version 4.3.3 (ruby 2.6.0-p0), codename: Mysterious Traveller
+# * Min threads: 5, max threads: 5
+# * Environment: development
+# * Listening on tcp://127.0.0.1:3000
+# * Listening on tcp://[::1]:3000
+# Use Ctrl-C to stop
+
+# По адресу http://localhost:3000/
+# смотрим стартовую картинку RUBY on RAILS
+# здесь же видны версии Rails и Ruby
+# у меня вот так:
+# Rails version: 6.0.3
+# Ruby version: ruby 2.6.0p0 (2018-12-25 revision 66547) [x86_64-linux]
 
 4. Делаем репозиторий в github.com
 
-4.1. На сайте создаём пустой репозиторий с именем, как имя главной папки нашего приложения.
+____ 4.1. На сайте создаём пустой репозиторий с именем, как имя главной папки нашего приложения.
 
-4.2. Далее запускаем в терминале, открытом внутри основной папки приложения 
+____ 4.2. Далее запускаем в терминале, открытом внутри основной папки приложения 
 
-echo "# Bad-App" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/OPR-A-RUBY/Bad-App.git
-git push -u origin master
+# echo "# Bad-App" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git remote add origin https://github.com/OPR-A-RUBY/Bad-App.git
+# git push -u origin master
 
-Аналогичные эффект должен получиться через ungit 
-Здесь можно подключ нашу папку к репозиторию через Fetch from origin
-вводим имя: origin
-url: git@github.com:OPR-A-RUBY/ZZ-Blog.git
-Внимание! именно SSH иначе придётся вводить логин и пароль
+# Аналогичные эффект должен получиться через ungit 
+# Здесь можно подключ нашу папку к репозиторию через Fetch from origin
+# вводим имя: origin
+# url: git@github.com:OPR-A-RUBY/ZZ-Blog.git
+# Внимание! именно SSH иначе придётся вводить логин и пароль
 
-4.3. Запускаем ungit 
+____ 4.3. Запускаем ungit 
 
-5. Создаём контроллер Home # index
-Далее все команды выполняются в терминале открытом в главной папке приложения
+____ 5. Создаём контроллер Home # index
+# Далее все команды выполняются в терминале открытом в главной папке приложения
 
-# rails g controller home index
+rails g controller home index
+# Здесь сщздаётся контроллер home с экшеном index 
+# Экшен это акция
 
-Так мы создали контроллер который будет выполняться при обращении к странице 
+# Так мы создали контроллер который будет выполняться при обращении к странице 
 http://localhost:3000/home/index
 
-6. Создаём модель, с набором свойств
+____ 6. Создаём модель Zamet, с набором свойств
 
-# rails g model Zamet z_title:string z_body:text
+rails g model Zamet z_title:string z_body:text
+# Running via Spring preloader in process 8453
+#       invoke  active_record
+#       create    db/migrate/20200514173342_create_zamets.rb
+#       create    app/models/zamet.rb
+#       invoke    test_unit
+#       create      test/models/zamet_test.rb
+#       create      test/fixtures/zamets.yml
 
-Проверяем наличие миграции в db.
 
-# rails db:migrate
+# Проверяем наличие новых файлов.
 
-Проверяем наличие таблицы в db.
+rails db:migrate
+
+# Проверяем наличие таблицы в db.
+
+____ 7. Настраиваем маршруты
+
+rake routes
+# выводит нам все существующие маршруты и контроллеры с экшенами к ним
+
+# например одна строка описывает один маршрут:
+#
+#   /------------------------ название переменной маршрута
+#   |        /--------------- метод 
+#   |        |        /------ маршрут 
+#   |        |        |                         /---------- контроллер
+#   V        V        V                         V    V----- экшен 
+# home_index GET    /home/index(.:format)       home#index
+
+# файл настройки маршрутов можно увидеть в config/routes.rb
 
